3833872e7c6d595534c2726c81b0d3428289ede1
Fernando Custodio <fercustodio.github@gmail.com>
2024-08-07 03:25:19 -0500

    creando const

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const ingreseTexto = document.getElementById("ingreseTexto");
+const botonEncriptar = document.getElementById("botonEncriptar");
+const botonDesencriptar = document.getElementById("botonDesencriptar");
+const botonCopiar = document.getElementById("botonCopiar");
+const mensajeFinal = document.getElementById("mensajeFinal");
+
+
+
+botonEncriptar.addEventListener("click",()=>{
+    console.log(ingreseTexto.value)
+})
+
+
+
+
+
