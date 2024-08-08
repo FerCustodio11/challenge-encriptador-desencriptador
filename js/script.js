e3e3c4d88683a86bb4d3877fb682074611524be3
Fernando Custodio <fercustodio.github@gmail.com>
2024-08-08 00:10:20 -0500

    ultimando detalles visuales

--- js/script.js
+++ js/script.js
@@ -8,6 +8,33 @@ const derechaInfo = document.getElementById("derechaInfo");
 const derecha = document.getElementById("derecha");
 
 
+const remplace = (newtext) => {
+    mensajeFinal.innerHTML = newtext;
+    mensajeFinal.classList.add("ajustar");
+    derecha.classList.add("ajuste")
+    leftTexarea.value ="";
+    leftTexarea.style.height = "auto";
+    leftTexarea.placeholder = "Ingrese el texto aqui";
+    munecoId.classList.add("ocultar");
+    derechaInfo.classList.add("ocultar");
+    botonCopiar.classList.remove("btn__ocultar");
+}
+
+
+const reset =() =>{
+    leftTexarea.value = "";
+    leftTexarea.style.height = "auto";
+    mensajeFinal.innerHTML="";
+    derecha.classList.remove("ajuste");
+    mensajeFinal.classList.remove("ajustar");
+    munecoId.classList.remove("ocultar");
+    mensajeFinal.placeholder = "Ningun mensaje fue encontrado";
+    derechaInfo.classList.remove("ocultar");
+    botonCopiar.classList.add("btn__ocultar");
+    leftTexarea.focus();
+}
+
+
 /*
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
@@ -26,48 +53,57 @@ let remplazar = [
 
 
 botonEncriptar.addEventListener("click",()=>{
-    const texto = leftTexarea.value.toLowerCase()
-    function encriptar(newText) {
-        for(let i = 0; i <remplazar.length; i++){
-            if (newText.includes(remplazar[i][0])){
-                newText = newText.replaceAll(remplazar[i][0], remplazar[i][1]);
-            };
-        };
-        return newText;
-    }
-    const textoEncriptado = encriptar(texto);
-    
-    mensajeFinal.innerHTML = textoEncriptado;
-    
-    munecoId.style.display = "none";
-    derechaInfo.style.display = "none";
-    botonCopiar.style.display = "block";
-    derecha.classList.add("ajustar");
-    mensajeFinal.classList.add("ajustar")
 
-});
-
-botonDesencriptar.addEventListener("click", () => {
     const texto = leftTexarea.value.toLowerCase();
-    function desencriptar(newText) {
-        for (let i = 0; i < remplazar.length; i++){
-            if (newText.includes(remplazar [i][1])){
-                newText = newText.replaceAll(remplazar[i][1], remplazar[i][0]);
-            };
+
+    if (texto != "" ){
+        function encriptar(newText) {
+            for(let i = 0; i <remplazar.length; i++){
+                if (newText.includes(remplazar[i][0])){
+                    newText = newText.replaceAll(remplazar[i][0], remplazar[i][1]);
+                };
+            };    
+            return newText;
         };
-        return newText;
-    }
-    const textoDesencriptado = desencriptar(texto);
-
-    mensajeFinal.innerHTML = textoDesencriptado;
-
+        remplace(encriptar(texto));
+    } else{
+        alert("Ingrese texto a encriptar");
+        reset();
+    };
 });
+
+
+
+
+
+
+botonDesencriptar.addEventListener("click",()=>{
+
+    const texto = leftTexarea.value.toLowerCase();
+
+    if (texto != "" ){
+        function encriptar(newText) {
+            for(let i = 0; i <remplazar.length; i++){
+                if (newText.includes(remplazar[i][1])){
+                    newText = newText.replaceAll(remplazar[i][1], remplazar[i][0]);
+                };
+            };    
+            return newText;
+        };
+        remplace(encriptar(texto));
+    } else{
+        alert("Ingrese texto a desencriptar");
+        reset();
+    };
+});
+
 
 botonCopiar.addEventListener("click", () => {
     let texto = mensajeFinal;
     texto.select();
     document.execCommand("copy");
     alert("texto copiado");
+    reset();
 });
 
 // ajuste textarea
